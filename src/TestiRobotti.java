0967da9a225d213838effd46896695b9bd48bac1
Johannes Vainio <vainjo4@gmail.com>
2012-10-10 13:20:30 +0200

    ääkköset pois
    .,-.

--- src/TestiRobotti.java
+++ src/TestiRobotti.java
@@ -1,6 +1,6 @@
 
 public class TestiRobotti {
 	public static void main( String[] main ) {
-		System.out.println( "Testibotti on nyt täällä!" );
+		System.out.println( "Testibotti on nyt taalla!" );
 	}
 }
