b9f4ed9177a31735b1b155adf3b92316c5cba6c1
Johannes Vainio <vainjo4@gmail.com>
2012-10-15 18:23:15 +0200

    Algoritmi v. 0.1
    raaka ja ruma, mutta alku kuitenkin

---
+++ src/Algoritmi.java
@@ -0,0 +1,124 @@
+
+
+
+import java.util.Arrays;
+
+public class Algoritmi {
+	
+	/*Alla oleva on karkea hutaisu, jossa on tavoiteltu oikeaa ideaa mutta kaikki muu on melko v‰‰rin. 
+	* Koodi olettaa ett‰ on olemassa RobotinMaailma.java, 
+	* jonne talletetaan ruudut joissa on k‰yty ja niiden tiedot.
+	*
+	*t‰m‰ algoritmi on siis kaikki mit‰ tehd‰‰n ennen askelta
+	*ts. robotin p‰‰tˆksentekoprosessi.
+	 */
+
+	private Ruutu edellinenRuutu; 
+	//^ t‰m‰ voisi oikeastaan kuulua robotille.
+	
+	//robotissa tehd‰‰n esim. seuraava temppu: 
+	//edellinenruutu = nykyinenruutu
+	//nykyinenruutu = uusiruutu
+	
+	
+	robo.ruutu.lisaaLaskuria();
+	//^ ruudulla (tai RobotinMaailmassa) on laskuri jota kasvatetaan aina kun
+	//ko. ruutuun menn‰‰n. T‰m‰kin rivi voi ehk‰ olla robotissa.
+
+	public void pyorahda() {
+		//T‰m‰ metodi tutkii naapurustosta mihin suuntiin voi menn‰.
+		
+		boolean voiEdetaPohjoiseen, 
+		voiEdetaItaan, voiEdetaEtelaan,
+		voiEdetaLanteen;
+
+		for (int i = 0; i < 4; i++) {
+			boolean voikoEdeta = robo.annaSuunta().voiEdeta();
+			robo.kaannyOikealle();	
+
+			boolean[] eteneminenSuunnittain = new boolean[4]; 
+			//^t‰m‰KIN on oikeastaan ruudun ominaisuus
+			
+			
+			
+			
+			
+			switch(robo.annaSuunta()) {
+			case 0 : { 
+				//eli case 0 tarkoittaa ett‰ robo katsoo pohjoiseen
+
+				if(voikoEdeta) {
+					eteneminenSuunnittain[0]= true;
+					
+				}
+				else eteneminenSuunnittain[0] = false;
+			}
+			case 1 : {
+				if(voikoEdeta) {
+					eteneminenSuunnittain[1]= true;
+				}
+				else eteneminenSuunnittain[1] = false;
+			}
+			case 2 : {
+				if(voikoEdeta) {
+					eteneminenSuunnittain[2]= true;
+				}
+				else eteneminenSuunnittain[2] = false;
+			}
+			case 3 : {
+				if(voikoEdeta) {
+					eteneminenSuunnittain[3]= true;
+				}
+				else eteneminenSuunnittain[3] = false;
+			}
+			}
+
+		}
+	
+		public int[] tutkaile() {
+		int[] naapurienLaskurit = new int[4];
+		int aaa;
+		
+		
+		
+		//alla on for-lause joka k‰y kaikki ilmansuunnat l‰pi ja tallettaa taulukkoon
+		for (aaa = 0; aaa < 4; aaa++) {
+	
+		if (robo.ruutu.eteneminenSuunnittain[aaa] = true) {
+		
+				if (RobotinMaailma.annaRuutu(/*suunnassa aaa*/) == null 
+						&& robo.ruutu.eteneminenSuunnittain[aaa] == true) {
+			
+					//eli jos naapuria ei tunneta (eli siell‰ ei ole k‰yty) mutta sinne voi edet‰, 
+					//sen laskurin katsotaan olevan 0 
+			
+					//johonkin tarvitaan metodi joka antaa RobonMaailmasta naapuriruudun suunnassa "aaa"
+					
+					naapurienLaskurit[aaa] = 0;
+				
+				}
+				else naapurienLaskurit[aaa] = RobotinMaailma.annaRuutu(/*suunnassa aaa*/).annaLaskurinArvo();
+				//muutoin kysyt‰‰n RobonMaailmalta ruudun laskurin arvo
+		
+		}
+		}
+		return naapurienLaskurit; //palautetaan taulukko, jossa indeksi (0-3) on ilmansuunta ja sen arvo on naapuriruudun laskurin arvo
+		}
+		
+	public int teeSuuntaPaatos() {
+		int[] naapurienLaskurit = tutkaile();	
+	int a = Arrays.binarySearch(naapurienLaskurit, 0);
+		if (a) {
+		return a;}
+	int b = Arrays.binarySearch(naapurienLaskurit, 1);
+		//pulma: miten tarkastaa lˆyt‰‰kˆ binary mit‰‰n? int: lle kun ei voi tehd‰ true/false tarkastelua 
+	}
+	
+	
+	}
+	public static void main(String[] args) {
+
+
+	}
+
+}
