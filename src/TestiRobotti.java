3f897fa580a59bb9653c36c05d7e0ab2ac10bf37
Jaakko Taskinen <jj.taskinen11@gmail.com>
2012-10-15 14:33:54 +0300

    Uusi printti
    botti fasilitoi

--- src/TestiRobotti.java
+++ src/TestiRobotti.java
@@ -2,6 +2,7 @@
 public class TestiRobotti {
 	public static void main( String[] main ) {
 		System.out.println( "Testibotti on taas taalla!!!" );
+		System.out.println( "Testibotti fasilitoi taas");
 	}
 	public String toString() {
 		return "Robotti";
