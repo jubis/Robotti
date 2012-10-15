3728f91e40ccf66fdf03c0f8adb59d5bb522a322
Emmi Linkola <emmi.linkola@gmail.com>
2012-10-15 14:27:40 +0300

    robotti
    jeejee

--- src/TestiRobotti.java
+++ src/TestiRobotti.java
@@ -3,4 +3,7 @@ public class TestiRobotti {
 	public static void main( String[] main ) {
 		System.out.println( "Testibotti on taas taalla!!!" );
 	}
+	public String toString() {
+		return "Robotti";
+	} 
 }
