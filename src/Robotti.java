d82bc7f8a0f693b8e249b62787b05962eb65d931
jubis <matias.saarinen@gmail.com>
2012-10-15 22:48:06 +0300

    kommentteja ylimääräiseen Robotti-luokkaan

--- src/Robotti.java
+++ src/Robotti.java
@@ -1,4 +1,11 @@
-
+/**
+ * Tämä luokka on vain sitä varten, että täällä määritellään 
+ * Robotti-luokan valmiit metodit. Tällä tavalla Eclipse ei herjaa siitä,
+ * että koodissa kutsuttaisi metodeja, joita ei ole määritelty.
+ * 
+ * @author Matias
+ *
+ */
 public class Robotti {
 	public boolean etene() {return true;}
 }
