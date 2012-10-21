ae553e6fca3c252e22f2165fa4c330aab095a362
jubis <matias.saarinen@gmail.com>
2012-10-21 16:22:40 +0300

    tarvittavien metodien otsikot lisätty
    Lisäsin luokkiin jo valmiiksi tarkat otsikot sekä kuvaukset metodeista,
    joita nyt pitäis sitten ruveta kirjottamaan.

--- src/RobotinMaailma.java
+++ src/RobotinMaailma.java
@@ -1,4 +1,19 @@
 
 public class RobotinMaailma {
-
+	
+	public static void siirry( int suunta ) {
+		//Muuttaa nykyiseksi ruuduksi annetussa suunnassa olevan naapurin
+	}
+	
+	public static RobotinRuutu annaSijainti() {
+		//palauttaa nykyisen ruudun
+	}
+	
+	public static RobotinRuutu annaNaapuri( int suunta ) {
+		//Antaa RobonMaailmasta naapuriruudun suunnassa "suunta". 
+		//Palauttaa null, jos suunnassa ei ole tunnettua ruutua.
+	}
+	
+	
+	
 }
--- src/RobotinRuutu.java
+++ src/RobotinRuutu.java
@@ -1,4 +1,18 @@
 
 public class RobotinRuutu {
-
+	public int annaLaskurinArvo() {
+		//palauttaa ruudun laskurin arvon
+	}
+	
+	public void lisaaLaskuria() {
+		//tallentaa tiedon laskarin lisämisestä oikeaan ruutuun
+	}
+	
+	public void asetaVoikoEdeta( int suunta, boolean voikoEdeta ) {
+		//tallentaa ruudun tietoihin, mihin suuntiin tästä ruudusta pääsee
+	}
+	
+	boolean voikoSuuntaanEdeta( int suunta ) {
+		//palauttaa, voiko tästä ruudusta kulkea annettuun suuntaa
+	}
 }
