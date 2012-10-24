5527973559a6ff80acf9c5db9a1a6234b278f08b
Matias Saarinen <matias.saarinen@aalto.fi>
2012-10-24 22:13:26 +0300

    apua koodaamiseen
    Lisäsin kommentteina apuja, joiden avulla siirry-metodissa pääsee
    eteenpäin.

--- src/RobotinMaailma.java
+++ src/RobotinMaailma.java
@@ -11,6 +11,7 @@ public class RobotinMaailma {
 	 * 
 	 */
 	
+	// Luo ja alusta Map<Point, RobotinRuutu> tässä
 	private static Map <Point, RobotinRuutu> ruudut;
 	
 	
@@ -20,13 +21,32 @@ public class RobotinMaailma {
 	
 	
 	public static void siirry( int suunta ) {
-		ruudut=new HashMap <Point,RobotinRuutu>();
+		// et halua alustaa Mappia tässä enää uudestaan
+		ruudut=new HashMap <Point,RobotinRuutu>(); 
+		
+		/**
+		 * Tässä kohdassa pitää laskea, mikä uusiRuutu on.
+		 * Siinä kannattaa ensin muuttaa nykyistäRuutua Point-olion 
+		 * translate-metodin avulla ja 
+		 * lopuksi laittaa apumuuttujan arvo uudenRuudun arvoksi.
+		 */
+		
+		/**
+		 * suunta-parametrin arvoilla voi tutkia suuntaa näin
+		 * 
+		 * suunta == Olo2Robotti.ITA tai
+		 * suunta == Olo2Robotti.LANSI
+		 * 
+		 */
+		
 		if (ruudut.containsValue(uusiRuutu)){
-		ruudut.get(uusiRuutu);
+			ruudut.get(uusiRuutu);
 		}else {
+			// tähän voit (kysymysmerkkien tilalle) luoda vaan uuden olion
+			// RobotinRuudusta
 			ruudut.put(uusiRuutu,???);
 		}
-		uusiRuutu=nykyinenRuutu;
+		uusiRuutu=nykyinenRuutu; //pitäiskö tän olla toisin päin
 
 		//Muuttaa nykyiseksi ruuduksi sen ruudun, johon siirrytään
 		//*Huom* Tarvittaessa luo uuden ruudun, jos paikalla ei vielä ole sitä
