d08e9af641c838cbbc96e5b7ed4f4ffc6e214702
Matias Saarinen <matias.saarinen@aalto.fi>
2012-10-25 14:57:15 +0300

    suunta-muuttujat

--- src/Olo2Robotti.java
+++ src/Olo2Robotti.java
@@ -1,5 +1,9 @@
 
 public class Olo2Robotti extends Robotti {	
+	public static final int POHJOINEN = 0;
+	public static final int ITA = 1;
+	public static final int ETELA = 2;
+	public static final int LANSI = 3;
 	
 	public void teeSiirto() {
 		RobotinMaailma.siirry( suunta );
