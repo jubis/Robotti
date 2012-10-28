5ac6ae48ca82d4afd56939466bc2056bd3ed07d3
Matias Saarinen <matias.saarinen@aalto.fi>
2012-10-28 17:33:57 +0200

    parantelua
    Luokka on testauskunnossa

--- src/Olo2Robotti.java
+++ src/Olo2Robotti.java
@@ -5,21 +5,40 @@ public class Olo2Robotti extends Robotti {
 	public static final int ETELA = 2;
 	public static final int LANSI = 3;
 	
-	private Algoritmi alg = new Algoritmi();
+	private Algoritmi alg = null;
 	
 	public Olo2Robotti() {
-		this.hoidaRobotinAsettaminen();
+		RobotinMaailma.alusta();
+		this.alg = new Algoritmi( this );
 	}
 	
-	private void hoidaRobotinAsettaminen() {
-		this.alg.asetaRobotti( this );
+	public boolean voikoSuuntaanEdeta( int suunta ) {
+		while( this.annaSuunta() != suunta ) {
+			this.kaannyVasemmalle();
+		}
+		return this.voiEdeta();
 	}
-	
+
 	public void teeSiirto() {
+		int suunta = alg.teeSuuntaPaatos();
+		
+		while( this.annaSuunta() != suunta ) {
+			this.kaannyVasemmalle();
+			System.out.println( this + " kääntyi suuntaan " + this.annaSuunta());
+		}
+		
 		RobotinMaailma.siirry( suunta );
 		
-		System.out.println("Etene");
+		System.out.println( this + " etenee suuntaan: " + this.annaSuunta() );
 		this.etene();
 	}
-	
+
+	public static void main( String[] args ) {
+		Olo2Robotti robo = new Olo2Robotti();
+		while(true) {
+			robo.teeSiirto();
+		}
+		
+	}
 }
+
