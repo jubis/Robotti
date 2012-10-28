f30eeef58e4d08ca7601ca22512f89d64bfe2b45
Matias Saarinen <matias.saarinen@aalto.fi>
2012-10-28 16:05:27 +0200

    robotille ilmentymä algoritmistä

--- src/Olo2Robotti.java
+++ src/Olo2Robotti.java
@@ -5,6 +5,16 @@ public class Olo2Robotti extends Robotti {
 	public static final int ETELA = 2;
 	public static final int LANSI = 3;
 	
+	private Algoritmi alg = new Algoritmi();
+	
+	public Olo2Robotti() {
+		this.hoidaRobotinAsettaminen();
+	}
+	
+	private void hoidaRobotinAsettaminen() {
+		this.alg.asetaRobotti( this );
+	}
+	
 	public void teeSiirto() {
 		RobotinMaailma.siirry( suunta );
 		
